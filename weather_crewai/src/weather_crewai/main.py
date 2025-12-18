5d16d34d64c0ec93d76a5e0fe49164f6dc5497b5
Ahmed Alghoul <amghoul@gmail.com>
2025-12-18 12:43:04 +0200

    update

--- weather_crewai/src/weather_crewai/main.py
+++ weather_crewai/src/weather_crewai/main.py
@@ -1,40 +1,42 @@
 #!/usr/bin/env python
 import sys
 import warnings
-from datetime import datetime
+
 from weather_crewai.crew import WeatherCrewai
 
 warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
+
+# This main file is intended to be a way for you to run your
+# crew locally, so refrain from adding unnecessary logic into this file.
+# Replace with your desired variables if they change
 
 def run():
     """
     Run the crew.
     """
-    # Get user input for the city
-    user_city = input("Enter the city you want a weather report for: ")
+    # Ask the user for the city input dynamically
+    city_input = input("Enter the city you want a weather report for: ")
 
     inputs = {
-        'city': user_city,
-        'current_year': str(datetime.now().year)
+        'city': city_input
     }
-
+    
     try:
         WeatherCrewai().crew().kickoff(inputs=inputs)
     except Exception as e:
         raise Exception(f"An error occurred while running the crew: {e}")
 
+
 def train():
     """
     Train the crew for a given number of iterations.
     """
-    user_city = input("Enter the city for training: ")
-
     inputs = {
-        'city': user_city,
-        'current_year': str(datetime.now().year)
+        "city": "London"
     }
     try:
         WeatherCrewai().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
+
     except Exception as e:
         raise Exception(f"An error occurred while training the crew: {e}")
 
@@ -44,21 +46,22 @@ def replay():
     """
     try:
         WeatherCrewai().crew().replay(task_id=sys.argv[1])
+
     except Exception as e:
         raise Exception(f"An error occurred while replaying the crew: {e}")
 
 def test():
     """
-    Test the crew execution and returns the results.
+    Test the crew execution and returns the expected output.
     """
-    user_city = input("Enter the city for testing: ")
-
     inputs = {
-        "city": user_city,
-        "current_year": str(datetime.now().year)
+        "city": "London"
     }
-
     try:
-        WeatherCrewai().crew().test(n_iterations=int(sys.argv[1]), eval_llm=sys.argv[2], inputs=inputs)
+        WeatherCrewai().crew().test(n_iterations=int(sys.argv[1]), openai_model_name=sys.argv[2], inputs=inputs)
+
     except Exception as e:
         raise Exception(f"An error occurred while testing the crew: {e}")
+
+if __name__ == "__main__":
+    run()
